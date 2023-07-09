897fc8a83a41a3cb7596f5dc26c04235902b35c4
SouravBandyopadhyay <souravb.1998@gmail.com>
2023-07-10 00:04:09 +0530

    changes in local

--- src/app/Components/AboutMe.js
+++ src/app/Components/AboutMe.js
@@ -1,51 +1,34 @@
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
-const AboutSection = () => {
+const AboutMeSection = () => {
   const aboutRef = useRef(null);
 
   useEffect(() => {
     const aboutSection = aboutRef.current;
-    if (!aboutSection) return;
+    const title = aboutSection.querySelector('.title');
+    const description = aboutSection.querySelector('.description');
 
-    const content = aboutSection.querySelector(".content");
-    const background = aboutSection.querySelector(".background");
+    gsap.set([title, description], { opacity: 0, y: 50 });
 
-    gsap.set(content, { opacity: 0, y: 100 });
-    gsap.set(background, { yPercent: -20 });
+    const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-    tl.to(content, { opacity: 1, y: 0, duration: 1 }).to(background, {
-      yPercent: 0,
-      duration: 1,
-    });
+    tl.to(title, { opacity: 1, y: 0, duration: 0.6 })
+      .to(description, { opacity: 1, y: 0, duration: 0.6 }, '-=0.3');
   }, []);
 
   return (
-    <section ref={aboutRef} className="relative py-16">
+    <section ref={aboutRef} className="py-16">
       <div className="container mx-auto px-4">
-        <div className="relative z-10">
-          <h1 className="text-4xl font-bold mb-4">About Me</h1>
-          <p className="text-lg text-gray-600 mb-6">
-            I am a Web Developer from Gurgaon, Haryana. I have gone through a
-            rigorous and competitive coding curriculum and learned MERN Stack at
-            Masai School. I have done various Collaborative as well as
-            Individual projects in my coding journey. Check out some of my work
-            in the Projects section
-          </p>
-          <p className="text-lg text-gray-600">
-            I am open to job opportunities where I can contribute, learn, and
-            grow. If you have a good opportunity that matches my skills and
-            experience, then don't hesitate to Contact me
+        <div className="text-center">
+          <h2 className="title text-4xl font-bold mb-4">About Me</h2>
+          <p className="description text-lg text-gray-600">
+            I am a Web Developer from Gurgaon, Haryana. I have gone through a rigorous and competitive coding curriculum and learned MERN Stack at Masai School. I have done various Collaborative as well as Individual projects in my coding journey. Check out some of my work in the Projects section.
           </p>
         </div>
-      </div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 overflow-hidden">
-        <div className="background absolute top-0 left-0 right-0 bottom-0" />
       </div>
     </section>
   );
 };
 
-export default AboutSection;
+export default AboutMeSection;
--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
         <LandingBanner />
         <HomePage />
         <AboutSection />
-        <SkillsSection/>
+        <SkillsSection />
       </main>
       {/* </CustomCursor> */}
     </>
