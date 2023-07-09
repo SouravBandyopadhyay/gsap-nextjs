b6d90b33853347a239c3358989ef5141d4ac764c
SouravBandyopadhyay <souravb.1998@gmail.com>
2023-07-10 00:45:28 +0530

    added about me and skill section

--- src/app/Components/AboutMe.js
+++ src/app/Components/AboutMe.js
@@ -1,29 +1,36 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 const AboutMeSection = () => {
   const aboutRef = useRef(null);
 
   useEffect(() => {
     const aboutSection = aboutRef.current;
-    const title = aboutSection.querySelector('.title');
-    const description = aboutSection.querySelector('.description');
+    const title = aboutSection.querySelector(".title");
+    const description = aboutSection.querySelector(".description");
 
-    gsap.set([title, description], { opacity: 0, y: 50 });
+    gsap.set([title, description], { opacity: 0, y: 200 });
 
-    const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-    tl.to(title, { opacity: 1, y: 0, duration: 0.6 })
-      .to(description, { opacity: 1, y: 0, duration: 0.6 }, '-=0.3');
+    tl.to(title, { opacity: 1, y: 0, duration: 0.6 }).to(
+      description,
+      { opacity: 1, y: 0, duration: 0.6 },
+      "-=0.3"
+    );
   }, []);
 
   return (
-    <section ref={aboutRef} className="py-16">
+    <section ref={aboutRef} className="mx-auto px-5 py-10 w-2/3 h-screen">
       <div className="container mx-auto px-4">
         <div className="text-center">
           <h2 className="title text-4xl font-bold mb-4">About Me</h2>
-          <p className="description text-lg text-gray-600">
-            I am a Web Developer from Gurgaon, Haryana. I have gone through a rigorous and competitive coding curriculum and learned MERN Stack at Masai School. I have done various Collaborative as well as Individual projects in my coding journey. Check out some of my work in the Projects section.
+          <p className="description text-2xl text-gray-600">
+            I am a Web Developer from Gurgaon, Haryana. I have gone through a
+            rigorous and competitive coding curriculum and learned MERN Stack at
+            Masai School. I have done various Collaborative as well as
+            Individual projects in my coding journey. Check out some of my work
+            in the Projects section.
           </p>
         </div>
       </div>
--- src/app/Components/SkillSection.js
+++ src/app/Components/SkillSection.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { FaJsSquare, FaPython, FaNpm } from "react-icons/fa";
-
+import {
+  FaJsSquare,
+  FaPython,
+  FaNpm,
+  FaHtml5,
+  FaReact,
+  FaHeart,
+  FaCheckCircle,
+} from "react-icons/fa";
+import { IoLogoJavascript, IoLogoCss3 } from "react-icons/io";
 const SkillsSection = () => {
   const skillsRef = useRef(null);
 
@@ -49,25 +57,27 @@ const SkillsSection = () => {
       skillsObserver.disconnect();
     };
   }, []);
-
+  const icons = [
+    { icon: <FaReact />, color: "text-blue-500", name: "ReactJS" },
+    { icon: <FaJsSquare />, color: "text-yellow-500", name: "JavaScript" },
+    { icon: <IoLogoCss3 />, color: "text-blue-300", name: "CSS3" },
+    { icon: <FaPython />, color: "text-red-500", name: "Python" },
+    { icon: <FaCheckCircle />, color: "text-green-500", name: "Circle" },
+  ];
   return (
-    <section ref={skillsRef} className="py-1">
+    <section ref={skillsRef} className="py-1 h-screen">
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-bold mb-8 text-center">Skills</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          <div className="skill-item bg-gray-800 rounded-lg p-4 shadow-md text-center">
-            <div className="text-2xl mb-4">JavaScript</div>
-            <FaJsSquare className="mx-auto h-12" />
-          </div>
-          <div className="skill-item bg-gray-800 rounded-lg p-4 shadow-md text-center">
-            <div className="text-2xl mb-4">Python</div>
-            <FaPython className="mx-auto h-12 " />
-          </div>
-          <div className="skill-item bg-gray-800 rounded-lg p-4 shadow-md text-center">
-            <div className="text-2xl mb-4">npm</div>
-            <FaNpm className="mx-auto h-12" />
-          </div>
-          {/* Add more skills here */}
+          {icons.map((el) => (
+            <div
+              key={el.id}
+              className="skill-item bg-gray-800 rounded-lg p-4 shadow-md text-center"
+            >
+              <div className="text-2xl mb-4">{el.name}</div>
+              <span className={`text-4xl ${el.color}`}>{el.icon}</span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
